ed500a76c17f2b77b6df2f6a1d4845e9aef4d2a0
Documenter.jl <documenter@juliadocs.github.io>
2022-09-14 17:54:38 +0000

    build based on 4522521

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"","page":"Home","title":"Home","text":"CurrentModule = TheGraphData","category":"page"},{"location":"#TheGraphData","page":"Home","title":"TheGraphData","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"Documentation for TheGraphData.","category":"page"},{"location":"","page":"Home","title":"Home","text":"","category":"page"},{"location":"","page":"Home","title":"Home","text":"Modules = [TheGraphData]","category":"page"}]
+}
