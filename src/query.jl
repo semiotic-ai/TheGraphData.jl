221c20f320e74819271d943858314076679a1139
Anirudh <anirudh@semiotic.ai>
2022-09-22 14:10:08 -0700

    fix: query return correct data type

--- src/query.jl
+++ src/query.jl
@@ -9,6 +9,10 @@ By default, the client is the gateway.
 This function returns a vector of dictionaries.
 """
 function query(v::AbstractString, a::Dict, f::AbstractVector{S}) where {S<:AbstractString}
-    vdata = @mock(GQLC.query(client[], v; query_args=a, output_fields=f)).data[v]
-    return vdata
+    # TODO: Better way to convert the type?
+    data::Vector{<:Dict} = map(  # Convert Vector{Dict{Any, Any}} to proper types
+        x -> x,
+        @mock(GQLC.query(client[], v; query_args=a, output_fields=f)).data[v],
+    )
+    return data
 end
