54f7fa198da8e61e39332e1dbad3d4652a5d5874
Anirudh <anirudh@semiotic.ai>
2022-09-28 11:49:54 -0700

    feat: read csv

--- src/io.jl
+++ src/io.jl
@@ -1,12 +1,26 @@
 """
+    splitextsym(f::AbstractString)
+
+Get the extension of the filepath `f` as a symbol.
+This function is unexported.
+"""
+function splitextsym(f::AbstractString)
+    x = Symbol(splitext(f)[end][2:end])  # TODO: Better way?
+    if x == Symbol("")
+        throw(ArgumentError("$f has no extension."))
+    end
+    return x
+end
+
+"""
     write(f::AbstractString, d; kwargs...)
     write(::Union{Val{:csv},Val{:txt}}, f::AbstractString, d::Union{T,D}; kwargs...) where {T<:Table,D<:Dict}
 
 Write the data `d` to the filepath `f` with kwargs.
-NOTE: This function is unexported.
+This function is unexported.
 """
 function write(f::AbstractString, d; kwargs...)
-    ext = Symbol(splitext(f)[end][2:end])  # TODO: Better way?
+    ext = splitextsym(f)
     try
         fout = write(Val(ext), f, d; kwargs...)
         return fout
@@ -24,4 +38,31 @@ function write(
     ::Union{Val{:csv},Val{:txt}}, f::AbstractString, d::Union{T,D}; kwargs...
 ) where {T<:Table,D<:Dict}
     @mock(CSV.write(f, d; kwargs...))
+end
+
+"""
+    read(f::AbstractString; kwargs...)
+    read(::Union{Val{:csv},Val{:txt}}, f::AbstractString; kwargs...)
+
+Read the data from the filepath `f` with kwargs.
+This function is unexported.
+"""
+function read(f::AbstractString; kwargs...)
+    # FIXME: Could become type unstable when we add more `read` function
+    # Check @code_warntype
+    ext = splitextsym(f)
+    try
+        d = read(Val(ext), f; kwargs...)
+        return d
+    catch e
+        if e isa MethodError
+            error(
+                "The function `read` does not support filetype $ext." *
+                "Please submit a feature request or PR if you want this functionality.",
+            )
+        end
+    end
+end
+function read(::Union{Val{:csv},Val{:txt}}, f::AbstractString; kwargs...)
+    @mock(CSV.File(f; kwargs...))
 end
--- test/io.jl
+++ test/io.jl
@@ -1,11 +1,15 @@
 @testset "io" begin
+    @testset "extsym" begin
+        @test TheGraphData.splitextsym("this/is/a/file.txt") == :txt
+        @test_throws ArgumentError TheGraphData.splitextsym("this/is/not/a/file")
+    end
     write_success_patch = @patch function CSV.write(f, t; kwargs...)
         println("write stub => simulating success")
         return "success!"
     end
     @testset "TheGraphData.write" begin
         @testset "failure" begin
-            @test_throws Exception TheGraphData.write("foo.bar", Dict("a" => 1))
+            @test_throws ErrorException TheGraphData.write("foo.bar", Dict("a" => 1))
         end
         @testset "table" begin
             d = Table(; a=[1, 2, 3], b=[3, 2, 1])
@@ -20,6 +24,16 @@
                 @test TheGraphData.write("fpath.csv", d; delimiter=" ") == "success!"
                 @test TheGraphData.write("fpath.txt", d; delimiter=" ") == "success!"
             end
+        end
+    end
+    read_csv_success_patch = @patch function CSV.File(f; kwargs...)
+        println("read stub => simulating success")
+        return CSV.File(IOBuffer("X\nb\nc\na\nc"))
+    end
+    @testset "TheGraphData.read" begin
+        apply(read_csv_success_patch) do
+            @test TheGraphData.read("fpath.csv")[1][:X] == "b"
+            @test_throws ErrorException TheGraphData.read("fpath.foo")
         end
     end
 end
