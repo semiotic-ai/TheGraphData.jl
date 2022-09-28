85df343a449bda7f7b33e8799f1614cd28070a7c
Anirudh <anirudh@semiotic.ai>
2022-09-28 15:34:40 -0700

    fix: fixes #16 closes #15

--- src/query.jl
+++ src/query.jl
@@ -11,9 +11,8 @@ This function returns a vector of dictionaries.
 function query(v::AbstractString, a::Dict, f::AbstractVector{S}) where {S<:AbstractString}
     # TODO: Better way to convert the type?
     # TODO: Catch MethodError: no method matching getindex(::Nothing, String)
-    data::Vector{<:Dict} = map(  # Convert Vector{Dict{Any, Any}} to proper types
-        x -> x,
-        @mock(GQLC.query(client[], v; query_args=a, output_fields=f)).data[v],
-    )
+    data::Vector{<:Dict} = Dict[
+        x for x in @mock(GQLC.query(client[], v; query_args=a, output_fields=f)).data[v]
+    ]
     return data
 end
