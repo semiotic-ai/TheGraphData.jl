2d674d72a541fae838c60417c92fb56fe1d92602
Anirudh <anirudh@semiotic.ai>
2023-03-20 14:13:51 -0700

    feat: Allow passing kwargs to mutate
    Signed-off-by: Anirudh <anirudh@semiotic.ai>

--- src/mutate.jl
+++ src/mutate.jl
@@ -11,7 +11,7 @@ Send a mutation to the client for value `v` with arguments `a`.
 By default, the client is the gateway.
 This function returns a GQL response.
 """
-function mutate(v::AbstractString, a::Dict)
-    d = @mock(GQLC.mutate(client[], v, a))
+function mutate(v::AbstractString, a::Dict; kwargs...)
+    d = @mock(GQLC.mutate(client[], v, a; kwargs...))
     return d
 end
